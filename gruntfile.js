62a1ce27a6f819d2ec6e4bdcd881e9c19f6a9c0f
johnthedesigner <john@johnthedesigner.com>
2015-07-11 23:18:25 -0400

    add video and style tweaks to home page

--- gruntfile.js
+++ gruntfile.js
@@ -2,74 +2,82 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        assemble: {
-            options: {
-                layout: 'page.hbs',
-                layoutdir: './src/templates/layouts',
-                partials: './src/templates/partials/**/*.hbs',
-                plugins: ['permalinks'],
-                data: './src/data/testdata.json',
-                assets: 'dist'
-            },
-            site: {
-                files: [{
-                    cwd: './src/views/',
-                    dest: './dist/',
-                    expand: true,
-                    src: '**/*.hbs'
-                }]
-            }
+      pkg: grunt.file.readJSON('package.json'),
+      assemble: {
+        options: {
+          layout: 'page.hbs',
+          layoutdir: './src/templates/layouts',
+          partials: './src/templates/partials/**/*.hbs',
+          plugins: ['permalinks'],
+          data: './src/data/testdata.json',
+          assets: 'dist'
         },
-        less: {
-            development: {
-                options: {
-                    compress: true,
-                    yuicompress: true,
-                    optimization: 2
-                },
-                files: {
-                    "./dist/css/styles.css": "./src/less/styles.less",
-                    "./dist/css/fontawesome.css": "./bower_components/fontawesome/less/font-awesome.less"
-                }
-            }
+        site: {
+          files: [{
+            cwd: './src/views/',
+            dest: './dist/',
+            expand: true,
+            src: '**/*.hbs'
+          }]
+        }
+      },
+      less: {
+        development: {
+          options: {
+            compress: true,
+            yuicompress: true,
+            optimization: 2
+          },
+          files: {
+            "./dist/css/styles.css": "./src/less/styles.less",
+            "./dist/css/fontawesome.css": "./bower_components/fontawesome/less/font-awesome.less"
+          }
+        }
+      },
+      concat: {
+        dist: {
+          src: ['./src/js/**/*.js'],
+          dest: './dist/js/scripts.js'
+        }
+      },
+      jshint: {
+        beforeconcat: ['./src/js/**/*.js'],
+        afterconcat: ['./dist/js/scripts.js']
+      },
+      watch: {
+        scripts: {
+          files: [
+          	'**/*.hbs',
+          	'./src/{,*/}*.less',
+          	'./src/{,*/}*.css',
+          	'./src/{,*/}*.js',
+          	'./src/{,*/}*.json'
+        	],
+          tasks: ['assemble','less','concat','jshint'],
+          options: {
+            spawn: false,
+            livereload: true
+          },
         },
-        concat: {
-            dist: {
-                src: ['./src/js/**/*.js'],
-                dest: './dist/js/scripts.js'
-            }
+      },
+      copy: {
+        main: {
+          expand: true,
+          cwd: './src/assets/',
+          src: '**',
+          dest: './dist/',
         },
-        jshint: {
-            beforeconcat: ['./src/js/**/*.js'],
-            afterconcat: ['./dist/js/scripts.js']
-        },
-        watch: {
-            scripts: {
-                files: [
-                	'**/*.hbs',
-                	'./src/{,*/}*.less',
-                	'./src/{,*/}*.css',
-                	'./src/{,*/}*.js',
-                	'./src/{,*/}*.json'
-            	],
-                tasks: ['assemble','less','concat','jshint'],
-                options: {
-                    spawn: false,
-                    livereload: true
-                },
-            },
-        },
-		connect: {
-			server: {
-				options: {
-					livereload:true,
-					open: true,
-					hostname:'localhost',
-					base:'./dist/'
-				}
-			}
-		}
+      },
+  		connect: {
+  			server: {
+  				options: {
+  					livereload:true,
+  					open: true,
+  					hostname:'localhost',
+  					base:'./dist/'
+  				}
+  			}
+  		}
     });
 
     // Load handlebars template compiler
@@ -79,9 +87,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['assemble','less','concat','jshint']);
-    grunt.registerTask('serve', ['assemble','less','concat','jshint','connect','watch']);
+    grunt.registerTask('default', ['assemble','less','concat','jshint','copy']);
+    grunt.registerTask('serve', ['assemble','less','concat','jshint','copy','connect','watch']);
 
 };
